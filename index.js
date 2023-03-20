4762de46e23c1e82b4dac2d4345c3110e467b4c0
Kadimane <kadimanegloriah@gmail.com>
2023-03-20 03:24:18 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Declare global variable customerName
+var customerName = "bob";
+
+// Define function upperCaseCustomerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Define function setBestCustomer
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+// Define function overwriteBestCustomer
+function overwriteBestCustomer() {
+  bestCustomer ='maybe bob';
+}
+
+// Declare global constant leastFavoriteCustomer
+const leastFavoriteCustomer = "Karen";
+
+// Define function changeLeastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  // Attempt to change the value of leastFavoriteCustomer
+  leastFavoriteCustomer = "Susan"; 
+}
